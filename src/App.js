f5c2766a28150edec4aa7f8f0ef9ab1a01b86989
Keri Ritenour <keri@keriritenour.com>
2023-03-22 10:17:00 -0400

    Added github link to footer and styling

--- src/App.js
+++ src/App.js
@@ -10,6 +10,20 @@ function App() {
         <h1 id="header">Weather App</h1>
         <Weather />
       </header>
+      <footer>
+        <p>
+          This project was coded by Keri Ritenour and is open-sourced on{" "}
+          <a
+            href="https://github.com/keri-codes"
+            target="_blank"
+            rel="noreferrer"
+            alt="link to profile page on github"
+          >
+            Github
+          </a>
+          .
+        </p>
+      </footer>
     </div>
   );
 }
